8308c464f9294de48042c51207bcc6405c04aa77
NickolasHKraus <NickHKraus@gmail.com>
2020-02-27 12:50:43 -0600

    Add test for `raise_for_status`

--- mock_requests/test/test_main.py
+++ mock_requests/test/test_main.py
@@ -1,20 +1,40 @@
 import unittest
 from unittest.mock import Mock, patch
 
+from requests.exceptions import HTTPError
+
 from .. import main
+
 
 class MainTestCase(unittest.TestCase):
     def setUp(self):
         super(MainTestCase, self).setUp()
-        self.mock_requests = patch.object(main, 'requests').start()
+        self.mock_requests = patch.object(main, "requests").start()
         self.mock_requests.get.return_value = Mock(
-            status_code=200, content=b'data'
+            status_code=200, content=b"data"
         )
+        self.url = "https://static-website.com/"
 
     def test_main(self):
-        mock_resp = main.main()
-        self.mock_requests.get.assert_called_with(
-            'https://static-website.com/'
+        resp = main.main()
+        self.mock_requests.get.assert_called_with(self.url)
+        self.assertEqual(200, resp.status_code)
+        self.assertEqual(b"data", resp.content)
+
+    def test_raise_http_error(self):
+        mock_resp = Mock(
+            status_code=400, content=b"error"
         )
-        self.assertEqual(200, mock_resp.status_code)
-        self.assertEqual(b'data', mock_resp.content)
+        mock_resp.raise_for_status.side_effect = HTTPError(
+            f"400 Client Error: Bad Request for url: {self.url}"
+        )
+        self.mock_requests.get.return_value = mock_resp
+        with self.assertRaises(HTTPError) as context:
+            resp = main.main()
+            self.mock_requests.get.assert_called_with(self.url)
+            self.assertEqual(400, resp.status_code)
+            self.assertEqual(b"error", resp.content)
+        self.assertEqual(
+            f"400 Client Error: Bad Request for url: {self.url}",
+            str(context.exception)
+        )
